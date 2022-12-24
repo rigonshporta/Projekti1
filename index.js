a2976b9d3552f31c61b8a6a770b2b9b5ca806478
Rigon Shporta <rigonsh@gmail.com>
2022-12-24 18:02:58 +0100

    latest

---
+++ index.js
@@ -0,0 +1,27 @@
+const emri = document.getElementById("emri");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("regjistrohu", (e) => {
+  let messages = [];
+  if (emri.value === "" || emri.value == null) {
+    messages.push("Emri???");
+  }
+
+  if (password.value.length <= 7) {
+    messages.push("Passwordi duhet te ket mbi 7 karaktere");
+  }
+  if (password.value.length >= 20) {
+    messages.push("Passwordi duhet te ket me pak se 20 karaktere");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+  if (password.value === "greta") {
+    messages.push("Passwordi smund te jet greta");
+  }
+});
